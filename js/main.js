093a7353365c591550804b280da3b56de337c82a
irfanrizvidev <irfanrizvidev@gmail.com>
2019-11-18 09:10:26 +0000

    not working: top nav, active, color change nav.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+// $(".nav .nav-link").on("click", function(){
+//    $(".nav").find(".active").removeClass("active");
+//    $(this).addClass("active");
+// });
+
+// $(document).ready(function() {
+//   $('li.active').removeClass('active');
+//   $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+// });
+
+$(document).ready(function() {
+    // -----------------------------------------------------------------------
+    $.each($('#navbar').find('li'), function() {
+        $(this).toggleClass('active', 
+            window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
+    }); 
+    // -----------------------------------------------------------------------
+});
